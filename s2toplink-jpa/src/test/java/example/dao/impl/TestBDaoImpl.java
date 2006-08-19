28eb8699fad546357a9d01a88284cb7f1c46179b
da-yoshi <da-yoshi@508f3ae4-fbbb-43c4-960f-f4acc79b903f>
2006-08-19 00:39:23 +0000

    git-svn-id: https://www.seasar.org/svn/s2toplink/trunk@37 508f3ae4-fbbb-43c4-960f-f4acc79b903f

--- s2toplink-jpa/src/test/java/example/dao/impl/TestBDaoImpl.java
+++ s2toplink-jpa/src/test/java/example/dao/impl/TestBDaoImpl.java
@@ -16,6 +16,8 @@
 package example.dao.impl;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -28,6 +30,7 @@ import example.entity.TestB;
  *
  */
 @Stateless
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
 public class TestBDaoImpl implements TestBDao {
     
     @PersistenceContext(unitName = "persistenceUnit")
