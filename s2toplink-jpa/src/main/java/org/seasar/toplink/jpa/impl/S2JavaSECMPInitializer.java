cab6aaa0e1fcd1011f92502f319009f0f22af44b
da-yoshi <da-yoshi@508f3ae4-fbbb-43c4-960f-f4acc79b903f>
2006-11-07 16:12:27 +0000

    git-svn-id: https://www.seasar.org/svn/s2toplink/trunk@85 508f3ae4-fbbb-43c4-960f-f4acc79b903f

--- s2toplink-jpa/src/main/java/org/seasar/toplink/jpa/impl/S2JavaSECMPInitializer.java
+++ s2toplink-jpa/src/main/java/org/seasar/toplink/jpa/impl/S2JavaSECMPInitializer.java
@@ -1,17 +1,13 @@
 package org.seasar.toplink.jpa.impl;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import oracle.toplink.essentials.ejb.cmp3.persistence.SEPersistenceUnitInfo;
 import oracle.toplink.essentials.internal.ejb.cmp3.JavaSECMPInitializer;
 import oracle.toplink.essentials.logging.AbstractSessionLog;
 
-import org.seasar.framework.autodetector.ResourceAutoDetector;
 import org.seasar.framework.container.S2Container;
 import org.seasar.framework.container.factory.S2ContainerFactory;
-import org.seasar.framework.util.tiger.CollectionsUtil;
 import org.seasar.toplink.jpa.PersistenceUnitInfoFactory;
 
 public class S2JavaSECMPInitializer extends JavaSECMPInitializer {
@@ -39,34 +35,10 @@ public class S2JavaSECMPInitializer extends JavaSECMPInitializer {
     
     private PersistenceUnitInfoFactory persistenceUnitInfoFactory;
     
-    protected Map<String, List<ResourceAutoDetector>> mappingFileAutoDetectors =
-        CollectionsUtil.newHashMap();
-
     public void setPersistenceUnitInfoFactory(
             PersistenceUnitInfoFactory persistenceUnitInfoFactory) {
         this.persistenceUnitInfoFactory = persistenceUnitInfoFactory;
     }
-
-    public void setMappingFileAutoDetector(
-            final ResourceAutoDetector[] resourceAutoDetectors) {
-        for (final ResourceAutoDetector detector : resourceAutoDetectors) {
-            addMappingFileAutoDetector(detector);
-        }
-    }
-
-    public void addMappingFileAutoDetector(final ResourceAutoDetector detector) {
-        addMappingFileAutoDetector(null, detector);
-    }
-
-    public void addMappingFileAutoDetector(final String unitName,
-            final ResourceAutoDetector detector) {
-        if (!mappingFileAutoDetectors.containsKey(unitName)) {
-            mappingFileAutoDetectors.put(unitName,
-                    new ArrayList<ResourceAutoDetector>());
-        }
-        mappingFileAutoDetectors.get(unitName).add(detector);
-    }
-
 
     @Override
     protected boolean callPredeploy(SEPersistenceUnitInfo persistenceUnitInfo, Map m) {
