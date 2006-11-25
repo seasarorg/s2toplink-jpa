8e392d43e5e6e67647614c9ad32751e127079f36
da-yoshi <da-yoshi@508f3ae4-fbbb-43c4-960f-f4acc79b903f>
2006-11-25 14:46:10 +0000

    コメント追加
    git-svn-id: https://www.seasar.org/svn/s2toplink/trunk@124 508f3ae4-fbbb-43c4-960f-f4acc79b903f

--- s2toplink-jpa/src/main/java/org/seasar/toplink/jpa/unit/TopLinkEntityReader.java
+++ s2toplink-jpa/src/main/java/org/seasar/toplink/jpa/unit/TopLinkEntityReader.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.seasar.toplink.jpa.unit;
 
 import java.util.List;
@@ -15,6 +30,10 @@ import org.seasar.framework.jpa.unit.EntityReader;
 import org.seasar.toplink.jpa.metadata.TopLinkAttributeDesc;
 import org.seasar.toplink.jpa.metadata.TopLinkEntityDesc;
 
+/**
+ * @author Hidenoshin Yoshida
+ *
+ */
 public class TopLinkEntityReader implements EntityReader {
 
     protected final DataSet dataSet = new DataSetImpl();
@@ -56,38 +75,21 @@ public class TopLinkEntityReader implements EntityReader {
             }
             
         }
-//        final HibernateAttributeDesc[] attributes = getEntityDesc()
-//                .getAttributeDescs();
-//
-//        for (int i = 0; i < attributes.length; i++) {
-//            final HibernateAttributeDesc attribute = attributes[i];
-//
-//            if (!(attribute.isSelectable() && attribute.isReadTarget())) {
-//                continue;
-//            }
-//
-//            for (int j = 0; j < attribute.getTableNameSize(); j++) {
-//                final String tableName = attribute.getTableName(j);
-//                final String[] columnNames = attribute
-//                        .getColumnNames(tableName);
-//                final int[] sqlTypes = attribute.getSqlTypes();
-//
-//                assert columnNames.length == sqlTypes.length : columnNames.length
-//                        + ", " + sqlTypes.length;
-//
-//                final DataTable table = dataSet.getTable(tableName);
-//
-//                for (int k = 0; k < sqlTypes.length; k++) {
-//                    final String columnName = columnNames[k];
-//                    if (columnName == null || table.hasColumn(columnName)) {
-//                        continue;
-//                    }
-//                    table.addColumn(columnName, ColumnTypes
-//                            .getColumnType(sqlTypes[k]));
-//                }
-//            }
-//        }
     }
+    
+//    protected void setupDiscriminatorColumn() {
+//        if (!getEntityDesc().hasDiscriminatorColumn()) {
+//            return;
+//        }
+//        final String tableName = getEntityDesc().getPrimaryTableName();
+//        final String columnName = getEntityDesc().getDiscriminatorColumnName();
+//        final DataTable table = dataSet.getTable(tableName);
+//        if (!table.hasColumn(columnName)) {
+//            final int sqlType = getEntityDesc().getDiscriminatorSqlType();
+//            table.addColumn(columnName, ColumnTypes.getColumnType(sqlType));
+//        }
+//    }
+
     
     protected TopLinkEntityDesc getEntityDesc() {
         return entityDesc;
