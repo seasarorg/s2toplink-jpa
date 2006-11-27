a87233c13617a49ceea8d5e666d27b0a6711b487
da-yoshi <da-yoshi@508f3ae4-fbbb-43c4-960f-f4acc79b903f>
2006-11-27 23:17:51 +0000

    git-svn-id: https://www.seasar.org/svn/s2toplink/trunk@135 508f3ae4-fbbb-43c4-960f-f4acc79b903f

---
+++ s2toplink-jpa/src/test/java/org/seasar/toplink/jpa/entity/Customer.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.toplink.jpa.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Version;
+
+/**
+ * @author Hidenoshin Yoshida
+ *
+ */
+@Entity
+public class Customer {
+    
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    private String name;
+    
+    private String address;
+    
+    private String phone;
+    
+    private Integer age;
+    
+    @Temporal(TemporalType.DATE)
+    private Date birthday;
+    
+    private Sex sex;
+    
+    @Version
+    private Integer version;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+}
---
+++ s2toplink-jpa/src/test/java/org/seasar/toplink/jpa/impl/MockTopLinkWeaver.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.toplink.jpa.impl;
+
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+import java.util.Map;
+
+import oracle.toplink.essentials.internal.weaving.TopLinkWeaver;
+import oracle.toplink.essentials.sessions.Session;
+
+/**
+ * @author Hidenoshin Yoshida
+ *
+ */
+public class MockTopLinkWeaver extends TopLinkWeaver {
+
+    /**
+     * @param session
+     * @param classDetailsMap
+     */
+    public MockTopLinkWeaver(Session session, Map classDetailsMap) {
+        super(session, classDetailsMap);
+    }
+
+    @Override
+    public byte[] transform(ClassLoader loader, String className, Class classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+        return classfileBuffer;
+    }
+
+
+}
