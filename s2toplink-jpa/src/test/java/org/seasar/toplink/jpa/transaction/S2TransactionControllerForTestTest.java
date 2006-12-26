3a04113a432dadba51c4309837651b2875911dbe
da-yoshi <da-yoshi@508f3ae4-fbbb-43c4-960f-f4acc79b903f>
2006-12-26 09:58:55 +0000

    git-svn-id: https://www.seasar.org/svn/s2toplink/trunk@188 508f3ae4-fbbb-43c4-960f-f4acc79b903f

---
+++ s2toplink-jpa/src/test/java/org/seasar/toplink/jpa/transaction/S2TransactionControllerForTestTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.toplink.jpa.transaction;
+
+import org.seasar.extension.jta.SingletonTransactionManagerProxy;
+import org.seasar.extension.unit.S2TestCase;
+
+/**
+ * @author hid-yoshida
+ *
+ */
+public class S2TransactionControllerForTestTest extends S2TestCase {
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        include("jpa.dicon");
+    }
+
+    /**
+     * {@link org.seasar.toplink.jpa.transaction.S2TransactionControllerForTest#acquireTransactionManager()} のためのテスト・メソッド。
+     * @throws Exception 
+     */
+    public void testAcquireTransactionManager() throws Exception {
+        S2TransactionControllerForTest controller = new S2TransactionControllerForTest();
+        assertEquals(SingletonTransactionManagerProxy.class, controller.acquireTransactionManager().getClass());
+    }
+
+}
