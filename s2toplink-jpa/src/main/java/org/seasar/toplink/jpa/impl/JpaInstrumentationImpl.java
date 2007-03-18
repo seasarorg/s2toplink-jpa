9d5fdf1545a4ecd49a12ca94db1b651ca34e0dce
da-yoshi <da-yoshi@508f3ae4-fbbb-43c4-960f-f4acc79b903f>
2007-03-18 10:50:46 +0000

    TopLinkWeavedクラスとEntityクラスの両方をロードできるClassLoaderに対して EntityのWeavingを行うように修正
    git-svn-id: https://www.seasar.org/svn/s2toplink/trunk@289 508f3ae4-fbbb-43c4-960f-f4acc79b903f

--- s2toplink-jpa/src/main/java/org/seasar/toplink/jpa/impl/JpaInstrumentationImpl.java
+++ s2toplink-jpa/src/main/java/org/seasar/toplink/jpa/impl/JpaInstrumentationImpl.java
@@ -16,10 +16,14 @@
 package org.seasar.toplink.jpa.impl;
 
 import java.io.InputStream;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.security.ProtectionDomain;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import javax.persistence.spi.ClassTransformer;
@@ -29,12 +33,13 @@ import oracle.toplink.essentials.internal.weaving.TopLinkWeaved;
 import oracle.toplink.essentials.internal.weaving.TopLinkWeaver;
 
 import org.seasar.framework.exception.ClassNotFoundRuntimeException;
+import org.seasar.framework.exception.IllegalAccessRuntimeException;
+import org.seasar.framework.exception.InvocationTargetRuntimeException;
 import org.seasar.framework.exception.NoSuchMethodRuntimeException;
 import org.seasar.framework.util.ClassLoaderUtil;
 import org.seasar.framework.util.ClassUtil;
 import org.seasar.framework.util.InputStreamUtil;
-import org.seasar.framework.util.ResourceUtil;
-import org.seasar.framework.util.tiger.ReflectionUtil;
+import org.seasar.framework.util.tiger.CollectionsUtil;
 import org.seasar.toplink.jpa.JpaInstrumentation;
 
 /**
@@ -45,14 +50,10 @@ public class JpaInstrumentationImpl implements JpaInstrumentation {
         
     private static final String DEFINE_CLASS_METHOD_NAME = "defineClass";
 
-    private static final String FIND_RESOURCE_METHOD_NAME = "findResource";
-
     private static final ProtectionDomain protectionDomain;
 
     private static Method defineClassMethod;
     
-    private static Method findResourceMethod;
-
     static {
         protectionDomain = AccessController
                 .doPrivileged(new PrivilegedAction<ProtectionDomain>() {
@@ -81,78 +82,159 @@ public class JpaInstrumentationImpl implements JpaInstrumentation {
             }
         });
 
-        AccessController.doPrivileged(new PrivilegedAction<Object>() {
-            public Object run() {
-                final Class<?>[] paramTypes = new Class[] {String.class};
-                try {
-                    final Class<?> loader = ClassUtil.forName(ClassLoader.class
-                            .getName());
-                    findResourceMethod = loader.getDeclaredMethod(
-                            FIND_RESOURCE_METHOD_NAME, paramTypes);
-                    findResourceMethod.setAccessible(true);
-                } catch (final NoSuchMethodException e) {
-                    throw new NoSuchMethodRuntimeException(ClassLoader.class,
-                            FIND_RESOURCE_METHOD_NAME, paramTypes, e);
-                }
-                return null;
-            }
-        });
     }
 
     /**
      * @see org.seasar.toplink.jpa.JpaInstrumentation#addTransformer(javax.persistence.spi.ClassTransformer,
      *      java.lang.ClassLoader)
      */
-    public void addTransformer(final ClassTransformer transformer, final ClassLoader ignore) {
-        if (!TopLinkWeaver.class.isInstance(transformer)) {
+    public void addTransformer(ClassTransformer classTransformer,
+            ClassLoader classLoader) {
+        if(!TopLinkWeaver.class.isInstance(classTransformer)) {
             return;
         }
-        final TopLinkWeaver weaver = TopLinkWeaver.class.cast(transformer);
+        TopLinkWeaver weaver = TopLinkWeaver.class.cast(classTransformer);
         @SuppressWarnings("unchecked")
-        final Map<String, ClassDetails> map = (Map<String, ClassDetails>) weaver
-                .getClassDetailsMap();
-        final ClassLoader loader =  TopLinkWeaved.class.getClassLoader();
-        for (final String classPath : map.keySet()) {
-            final String  className = classPath.replace("/", ".");
-            if (isLoaded(loader, className)) {
+        Map<String, ClassDetails> map = (Map<String, ClassDetails>) weaver.getClassDetailsMap();
+        for (String classPath : map.keySet()) {
+            String className = classPath.replace('/', '.');
+            if (isLoaded(classLoader, className)) {
                 continue;
             }
-            final InputStream is = ResourceUtil.getResourceAsStream(classPath + ".class");
-            byte[] bytes = null;
+            
+            ResourceData data = getResourceData(classLoader, classPath);
+            if (data == null) {
+                continue;
+            }
+            
+            byte[] temp = InputStreamUtil.getBytes(data.getIn());
             try {
-                bytes = InputStreamUtil.getBytes(is);
-            } finally {
-                InputStreamUtil.close(is);
-            }
-            final byte[] transBytes = transform(transformer, loader, classPath,
-                    null, null, bytes);
-            if (transBytes != null) {
-                ReflectionUtil.invoke(defineClassMethod, loader, className,
-                        transBytes, 0, transBytes.length, protectionDomain);
+                byte[] bytes = classTransformer.transform(data.getLoader(),
+                        classPath, null, protectionDomain, temp);
+                if (bytes != null) {
+                    defineClassMethod.invoke(
+                            data.getLoader(),
+                            className,
+                            bytes,
+                            0,
+                            bytes.length,
+                            protectionDomain);
+                }
+            } catch (IllegalAccessException e) {
+                if (data != null && data.getLoader() != null) {
+                    throw new IllegalAccessRuntimeException(data.getLoader().getClass(), e);
+                } else {
+                    throw new RuntimeException(e);
+                }
+            } catch (InvocationTargetException e) {
+                if (data != null && data.getLoader() != null) {
+                    throw new InvocationTargetRuntimeException(data.getLoader().getClass(), e);
+                } else {
+                    throw new RuntimeException(e);
+                }
+            } catch (IllegalClassFormatException e) {
+                throw new RuntimeException(e);
             }
         }
     }
 
-    // TODO
-    protected byte[] transform(final ClassTransformer transformer,
-            final ClassLoader loader, final String className,
-            final Class<?> classBeingRedefined,
-            final ProtectionDomain protectionDomain,
-            final byte[] classfileBuffer) {
-        try {
-            return transformer.transform(loader, className,
-                    classBeingRedefined, protectionDomain, classfileBuffer);
-        } catch (final Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    // TODO
+    /**
+     * 引数のClassLoaderで指定したクラスがロードできるかチェックします。
+     * @param loader チェック対象ClassLoader
+     * @param className チェック対象クラス名
+     * @return ロード可能の場合true
+     */
     protected boolean isLoaded(final ClassLoader loader, final String className) {
         try {
             return ClassLoaderUtil.findLoadedClass(loader, className) != null;
         } catch (final ClassNotFoundException e) {
             throw new ClassNotFoundRuntimeException(e);
+        }
+    }
+        
+    /**
+     * クラス名のStringとClassLoaderを渡して、ClassをロードできるClassLoaderとClassオブジェクトのInputStreamを取得します。
+     * 
+     * @param loader ClassLoaderオブジェクト
+     * @param className クラス名
+     * @return 指定したクラスをロードできるClassLoaderと、指定したクラスのInputStreamを持つResourceData
+     */
+    protected ResourceData getResourceData(ClassLoader loader, String className) {
+        className = className + ".class";
+        String weavedClassName = TopLinkWeaved.class.getName().replace('.', '/') + ".class";
+        for (ClassLoader cl : getClassLoaderList(loader)) {
+            if (cl.getResource(weavedClassName) == null) {
+                continue;
+            }
+            InputStream in = cl.getResourceAsStream(className);
+            if (in != null) {
+                ResourceData data = new ResourceData();
+                data.setLoader(cl);
+                data.setIn(in);
+                return data;
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * ClassLoaderを受け取り、引数のClassLoaderから親ClassLoaderを検索し、取得できた全てのClassLoaderをListで返します。
+     * 
+     * @param loader ClassLoaderオブジェクト
+     * @return 引数のClassLoaderを含み、取得出来る全ての親ClassLoaderを含むList。
+     *         親が子の前に来るように順番付けしている。
+     */
+    protected List<ClassLoader> getClassLoaderList(ClassLoader loader) {
+        LinkedList<ClassLoader> list = CollectionsUtil.newLinkedList();
+        list.add(loader);
+        while (loader.getParent() != null && loader.getParent() != loader) {
+            list.addFirst(loader.getParent());
+            loader = loader.getParent();
+        }
+        return list;
+    }
+    
+    /**
+     * InputStremとClassLoaderをセットで保持するクラス
+     * @author Hidenoshin Yoshida
+     *
+     */
+    protected class ResourceData {
+        
+        private InputStream in;
+        
+        private ClassLoader loader;
+
+        /**
+         * InputStreamオブジェクトを返します。
+         * @return in InputStreamオブジェクト
+         */
+        public InputStream getIn() {
+            return in;
+        }
+
+        /**
+         * InputStreamオブジェクトを設定します。
+         * @param in 設定するInputStreamオブジェクト
+         */
+        public void setIn(InputStream in) {
+            this.in = in;
+        }
+
+        /**
+         * ClassLoaderオブジェクトを返します。
+         * @return loader ClassLoaderオブジェクト
+         */
+        public ClassLoader getLoader() {
+            return loader;
+        }
+
+        /**
+         * ClassLoaderオブジェクトを設定します。
+         * @param loader 設定するClassLoaderオブジェクト
+         */
+        public void setLoader(ClassLoader loader) {
+            this.loader = loader;
         }
     }
 
